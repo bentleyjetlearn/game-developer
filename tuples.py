47d32bd3cbea8c00f7b7fd3a7ac688295f761158
bentleyjetlearn <bentleyron313@gmail.com>
2025-08-18 12:39:23 +0200

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,7 @@
+x=(1,2,3,4,5)
+print(type(x))
+print(x[3])
+print(x[2:5])
+address=(25,'Benja',"St.John's road", 'New york', 'England', 57863)
+(doornum, place, streetname, city, country, zipcode)=address
+print(country)
