83e0d84cfeb56040b0ff534afeb7fa65882cf43d
bentleyjetlearn <bentleyron313@gmail.com>
2025-08-25 14:58:36 +0200

    Add files via upload

--- satelitegame.py
+++ satelitegame.py
@@ -23,20 +23,22 @@ pgzrun.go()'''
 
 import pgzrun
 import random
-
+from time import time
 WIDTH = 600
 HEIGHT = 600
 total_sat = 10
 satelite = []
 sat_check=0
 lines=[]
-
+game_over=False
+start_time=time()
 for i in range(10):
     sat = Actor('satelite')
     sat.pos = random.randint(50, 550), random.randint(50, 550)
     satelite.append(sat)
 
 def draw():
+    global displaytime
     screen.blit('space', (0, 0))
     num = 0
     for sat in satelite:
@@ -45,6 +47,14 @@ def draw():
         screen.draw.text(str(num), (sat.x, sat.y - 20), color="white")
     for i in lines:
         screen.draw.line(i[0],i[1],'white')
+    if sat_check<10:
+        displaytime=time()-start_time
+        screen.draw.text(str(round(displaytime)),(10,580),color='white')
+    else:
+        screen.draw.text(str(round(displaytime)),(10,580),color='white')
+    if game_over:
+        screen.blit('space', (0, 0))
+        screen.draw.text('Game over',(100,100),color='red')
 
 def update():
     pass
@@ -58,6 +68,10 @@ def on_mouse_down(pos):
 
         else:
             lines=[]
-            sat_check=0    
+            sat_check=0 
 
+def finish():
+    global game_over
+    game_over=True
+clock.schedule(finish, 15)
 pgzrun.go()
